9a93e386b9359e6cde7e34c2fd2d3f728f58c1de
miguelhuerta <guelme88@gmail.com>
2023-06-12 19:22:25 -0700

    finalizing docker containers

--- src/services/config/env-config.service.ts
+++ src/services/config/env-config.service.ts
@@ -23,6 +23,22 @@ export class EnvConfigService implements ConfigInterface {
     return this.configService.get<number>('DATABASE_PORT');
   }
 
+  public getDatabaseUser(): string {
+    return this.configService.get<string>('DATABASE_USER');
+  }
+
+  public getDatabasePassword(): string {
+    return this.configService.get<string>('DATABASE_PASSWORD');
+  }
+
+  public getDatabaseHost(): string {
+    return this.configService.get<string>('DATABASE_HOST');
+  }
+
+  public getDatabaseName(): string {
+    return this.configService.get<string>('DATABASE_NAME');
+  }
+
   /**
    * Get App URL
    * @return string
